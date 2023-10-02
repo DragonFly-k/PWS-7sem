7ef99554b29d0ad4c00ecda3afcacf2da0569486
Kate <2003katy2003@mail.ru>
2023-10-02 08:26:26 +0300

    .

---
+++ 1/PWS_1/Handlers/PutHandler.cs
@@ -0,0 +1,31 @@
+﻿using PWS_1.Model;
+using System.Web;
+using System.Web.SessionState;
+
+namespace PWS_1.Handlers
+{
+    public class PutHandler : IHttpHandler, IRequiresSessionState
+    {
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            var add = int.Parse(request.Params["ADD"]);
+            
+            var dataStorage = (Storage)context.Session["Storage"];
+
+            if (dataStorage == null)
+            {
+                dataStorage = new Storage();
+                context.Session["Storage"] = dataStorage;
+            }
+
+            dataStorage.Stack.Push(add);
+            
+            response.Write("Last element from stack: " + add);
+        }
+    }
+}
