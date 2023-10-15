42935453005e6f04319a4bc6593627cb79836060
Kate <2003katy2003@mail.ru>
2023-10-15 15:25:00 +0300

    .

---
+++ 2/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+using lab2.Models;
+
+namespace lab2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        private static readonly Storage Storage = new();
+        public string Get()
+        {
+            var resultValue = Storage.Result;
+            var topStackElement = Storage.Stack.Count > 0 ? Storage.Stack.Peek() : 0;
+            if (Storage.Stack.Count == 0)
+            {
+                return "{\"result\": " + resultValue + "}";
+            }
+            else
+            {
+                return "{\"result\": " + (topStackElement+ resultValue) + "}";
+            }
+        }
+        public void Post(int result)
+        {
+            Storage.Result = result;
+        }
+        public void Put(int add)
+        {
+            Storage.Stack.Push(add);
+        }
+        public void Delete()
+        {
+            if (Storage.Stack.Count != 0)
+            {
+                Storage.Stack.Pop();
+            }
+        }
+    }
+}
---
+++ 2/Models/Storage.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace lab2.Models
+{
+    public class Storage
+    {
+        public Stack<int?> Stack = new();
+        public int? Result = 0;
+    }
+}
